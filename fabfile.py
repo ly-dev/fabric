34a80ada54ce67ae00d1745392449f250e4f85e4
Yong Li <dev@liyong.cn>
2016-06-26 22:57:58 +0100

    install apache2

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,15 @@
 from fabric.api import *
 
+PACKAGE_MANAGER_INSTALL = "apt-get install %s"
+PACKAGE_MANAGER_UPDATE = "apt-get update"
+
 def hello():
     run('echo "hello from `hostname`"')
+
+def installPackages(packages):
+    sudo(PACKAGE_MANAGER_UPDATE)
+    for package in packages:
+        sudo(PACKAGE_MANAGER_INSTALL % package)
+
+def installApache2():
+    installPackages(["apache2"])
